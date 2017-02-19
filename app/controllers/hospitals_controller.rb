9176eadb96647fe1dd8e79e73f28f769ec3f40c5
seitaro <seitaro.hrkw@gmail.com>
2017-02-19 19:35:51 +0900

    現在地からの検索を実装

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -1,19 +1,43 @@
 class HospitalsController < ApplicationController
   def index
+    @geolocation = featch_geolocation
     @hospitals = search_hospital(params)
     @markers = markers(@hospitals) unless(@hospitals.nil?)
     @area_options = %w"千葉 船橋 柏 銚子 木更津 茂原 成田 東金".map{|i|[i,i]}
+    @km_options = [%w"500m 1km 2km 3km 5km 10km", %w"0.5 1 2 3 5 10"].transpose
+  end
+
+  def set_geolocation
+    @km_options = [%w"500m 1km 2km 3km 5km 10km", %w"0.5 1 2 3 5 10"].transpose
+     if (params[:lat].present? or params[:lng].present?)
+       cookies[:geolocation] = {expires: 1.hour.from_now,
+                                value: JSON.generate(lat: params[:lat],lng: params[:lng])}
+       render :partial => 'hospitals/geolocation_area'
+     else
+       render "<div>取得失敗</div>"
+     end
   end
 
   private
   def search_hospital(params)
     rel = Hospital
+    rel = rel.within(params[:km].to_i,origin: @geolocation) if params[:km].present? and @geolocation.present?
     rel = rel.where('subject LIKE ?',"%#{params[:subject]}%") if params[:subject].present?
     rel = rel.where('name LIKE ?',"%#{params[:name]}%") if params[:name].present?
     rel = rel.where("jurisdiction = ?", params[:jurisdiction]) if params[:jurisdiction].present?
-    rel = rel.all if( not params[:subject].present? and not params[:jurisdiction].present? and not params[:name].present?)
+    rel = rel.all if(params[:subject].blank? and params[:jurisdiction].blank? and params[:name].blank? and params[:km].blank?)
     rel
   end
+
+  def featch_geolocation
+    if cookies[:geolocation].present?
+      values = JSON.parse(cookies[:geolocation])
+      [values['lat'].to_f,values['lng'].to_f]
+    else
+      []
+    end
+  end
+
   def markers(hospitals)
     Gmaps4rails.build_markers hospitals do|hospital, marker|
       marker.lat hospital.latitude
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'hospitals#index'
+  post 'hospitals/set_geolocation'
 end
