0790ed925eca2ef0af0d60ec1eb9a6580df261f2
seitaro <seitaro.hrkw@gmail.com>
2017-02-27 19:56:40 +0900

    aboutページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'static_pages/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'hospitals#index'
   post 'hospitals/set_geolocation'
