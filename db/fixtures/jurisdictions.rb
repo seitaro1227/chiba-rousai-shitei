05a6652d29161508a1af522ed16cd43b0e7a31cc
seitaro <seitaro.hrkw@gmail.com>
2017-03-25 20:16:07 +0900

    seed_fuでのjurisdictionsとhospitalsを修正
    jurisdictions
    ファイル名を修正
    複数回実行したときに重複して取り込まれる問題を修正
    
    hospitals
    :jurisdiction_idでjurisdictionsのidを設定するようにした

--- db/fixtures/jurisdictions.rb
+++ db/fixtures/jurisdictions.rb
@@ -1,5 +1,8 @@
 jurisdiction_map = Hash[*%w"chiba 千葉 kashiwa 柏 funabashi 船橋 mobara 茂原 togane 東金 choshi 銚子 narita 成田 kisarazu 木更津"]
 
 jurisdiction_map.each do|roman, name|
-  Jurisdiction.seed({roman: roman,name:name})
+  Jurisdiction.seed(:roman) do |s|
+    s.roman = roman
+    s.name = name
+  end
 end
