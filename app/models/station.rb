f80d430e66e651229c83d91a5f2efa2bc3ed23fd
seitaro <seitaro.hrkw@gmail.com>
2017-03-31 00:48:31 +0900

    Stationモデルを作成

---
+++ app/models/station.rb
@@ -0,0 +1,10 @@
+class Station < ApplicationRecord
+  geocoded_by :name
+  acts_as_mappable :default_units => :kms,
+                   :default_formula => :sphere,
+                   :distance_field_name => :distance,
+                   :lat_column_name => :latitude,
+                   :lng_column_name => :longitude
+
+  after_validation :geocode, if: :name_changed?
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322163334) do
+ActiveRecord::Schema.define(version: 20170330153410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(version: 20170322163334) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "roman"
+  end
+
+  create_table "stations", force: :cascade do |t|
+    t.string   "name"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
