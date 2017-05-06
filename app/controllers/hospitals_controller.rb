d7e8c6d6d70897b5789d78470f52e1f23384cdf4
seitaro <seitaro.hrkw@gmail.com>
2017-05-07 00:40:11 +0900

    hospitals.geojsonを生成する処理をjbuilderに移した

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -1,7 +1,7 @@
 class HospitalsController < ApplicationController
   def index
     jurisdiction = Jurisdiction.find_by(roman: params[:jurisdiction])
-    @current_location = location_params(params)
+    @current_location = Hospital.location_params(params)
     @subject_ids = []
     @subject_ids = params[:subjects][:ids] if params[:subjects].present?
     @jurisdictions = Jurisdiction.all
@@ -10,15 +10,13 @@ class HospitalsController < ApplicationController
     @hospitals = Hospital.search(params)
     @center_of_gravity = center_of_gravity(@hospitals)
     @stations = Station.all
-    @geojson = geojson(@hospitals)
     respond_to do |format|
       format.html
-      format.geojson {render :json => @geojson}
+      format.geojson
     end
   end
 
   private
-
   # 複数の病院の中心(重心)座標を返します
   # 複数の病院が空の場合は[NaN,NaN]が返ります
   def center_of_gravity(hospitals)
@@ -26,28 +24,5 @@ class HospitalsController < ApplicationController
     south, north = latlng.minmax { |x, y| x[0]<=>y[0] }
     east, west = latlng.minmax { |x, y| x[1]<=>y[1] }
     Geocoder::Calculations.geographic_center([east, south, west, north])
-  end
-
-  def geojson(hospitals)
-    features = hospitals.map do |hospital|
-      {:type => "Feature",
-       :geometry => {"type" => "Point", "coordinates" => [hospital.longitude, hospital.latitude]},
-       :properties => {
-           :number       => hospital.number,
-           :name         => hospital.name,
-           :address      => hospital.address,
-           :jurisdiction_name => hospital.jurisdiction.name,
-           :phone_number => hospital.phone_number,
-           :saikei       => hospital.saikei,
-           :niji         => hospital.niji,
-           :subject      => hospital.orgin_subject,
-           :zip_code     => hospital.zip_code,
-       }
-      }
-    end
-
-    {"type" => "FeatureCollection",
-     "features" => features
-    }
   end
 end
--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,17 +1,17 @@
 class Hospital < ApplicationRecord
-  has_many :hospital_subjects, :dependent => :destroy
-  has_many :subjects, :through => :hospital_subjects
-  validates :name, :presence => true
-  validates :address, :presence => true
-  validates :number, :presence => true, :uniqueness => true
-  validates :zip_code, :presence => true
+  has_many :hospital_subjects, dependent: :destroy
+  has_many :subjects, through: :hospital_subjects
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :number, presence: true, uniqueness: true
+  validates :zip_code, presence: true
 
   geocoded_by :address
-  acts_as_mappable :default_units => :kms,
-                   :default_formula => :sphere,
-                   :distance_field_name => :distance,
-                   :lat_column_name => :latitude,
-                   :lng_column_name => :longitude
+  acts_as_mappable default_units: :kms,
+                   default_formula: :sphere,
+                   distance_field_name: :distance,
+                   lat_column_name: :latitude,
+                   lng_column_name: :longitude
   after_validation :geocode, if: :address_changed?
   belongs_to :jurisdiction
 
