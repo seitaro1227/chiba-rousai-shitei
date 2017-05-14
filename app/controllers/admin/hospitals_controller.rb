bd0eaf66f7ea1b8fdc7064a03b833ecea62cfd6f
seitaro <seitaro.hrkw@gmail.com>
2017-05-14 18:33:32 +0900

    Admin::Hospitalsのviewを追加

--- app/controllers/admin/hospitals_controller.rb
+++ app/controllers/admin/hospitals_controller.rb
@@ -3,13 +3,11 @@ class Admin::HospitalsController < ApplicationController
   before_action :authenticate_admin
 
   # GET /admin/hospitals
-  # GET /admin/hospitals.json
   def index
-    @hospitals = Hospital.all
+    @hospitals = Hospital.includes(:subjects,:jurisdiction).all
   end
 
   # GET /admin/hospitals/1
-  # GET /admin/hospitals/1.json
   def show
   end
 
@@ -23,42 +21,36 @@ class Admin::HospitalsController < ApplicationController
   end
 
   # POST /admin/hospitals
-  # POST /admin/hospitals.json
   def create
     @hospital = Hospital.new(hospital_params)
     if @hospital.save
-      redirect_to [:admin, @hospital], notice: 'Hospital was successfully created.'
+      redirect_to [:admin, @hospital], notice: 'Hospitalを新規作成しました'
     else
+      flash.now[:alert] = "Hospital#createが失敗しました"
       render :new
     end
   end
 
   # PATCH/PUT /admin/hospitals/1
-  # PATCH/PUT /admin/hospitals/1.json
   def update
     if @hospital.update(hospital_params)
-      redirect_to [:admin, @hospital], notice: 'Hospital was successfully updated.'
+      redirect_to [:admin, @hospital], notice: "#{@hospital.name}を更新しました"
     else
+      flash.now[:alert] = "Hospital#updateが失敗しました"
       render :edit
     end
   end
 
   # DELETE /admin/hospitals/1
-  # DELETE /admin/hospitals/1.json
   def destroy
     @hospital.destroy
-    respond_to do |format|
-      format.html { redirect_to admin_hospitals_url, notice: 'Hospital was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to admin_hospitals_url, notice: "#{@hospital.name}を削除しました"
   end
 
   private
-    REALM = controller_name
-    USERS = { "admin" => Digest::MD5::hexdigest(["admin", controller_name, ENV['ADMIN_PASSWORD']].join(":")) }
-    # Use callbacks to share common setup or constraints between actions.
     def set_hospital
       @hospital = Hospital.find(params[:id])
+      @jurisdictions = Jurisdiction.all #Jurisdiction.pluck(:id,:name)
     end
 
     def hospital_params
@@ -66,7 +58,9 @@ class Admin::HospitalsController < ApplicationController
           .permit(:number, :name, :zip_code, :address, :saikei, :niji, :phone_number, :latitude, :longitude)
     end
 
+    REALM = controller_name
+    USERS = { "admin" => Digest::MD5::hexdigest(["admin", controller_name, ENV['ADMIN_PASSWORD']].join(":")) }
     def authenticate_admin
-      authenticate_or_request_with_http_digest(REALM) {|username| USERS[username] }
+      authenticate_or_request_with_http_digest(REALM) { |username| USERS[username] }
     end
 end
